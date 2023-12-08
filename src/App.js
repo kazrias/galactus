fdbcd4318c2b56ab5a15a9079662319a763bfe13
kazrias <ka6rias@gmail.com>
2023-12-08 20:26:06 +0400

    Change background, add animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Header } from "./components/Header/Header";
 import { FilterScreen } from "./components/FilterScreen/FilterScreen";
-
+import { Background } from "./components/Background/Background";
 import './app.css'
 const App = () => {
   return (
     <>
+      <Background />
       <Header />
       <FilterScreen />
     </>
---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,13 @@
+import './Background.css'
+export const Background = () => {
+  return (
+    <div>
+      <div className="bg"></div>
+      <div className="star-field">
+        <div className="layer"></div>
+        <div className="layer"></div>
+        <div className="layer"></div>
+      </div>
+    </div>
+  )
+}
