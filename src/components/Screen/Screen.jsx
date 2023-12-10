0d284a9a1b2842894f9eabdf854db3fe7e331aec
kazrias <ka6rias@gmail.com>
2023-12-10 20:25:15 +0400

    Add new font

--- src/components/Screen/Screen.jsx
+++ src/components/Screen/Screen.jsx
@@ -3,6 +3,6 @@ import './Screen.css'
 const btns = [{ id: 1, type: '' }]
 export const Screen = () => (
   <div className="filter-screen">
-    <h1>Galactus Store</h1>
+    <h1><span>G</span>alactus Store</h1>
   </div>
 )
