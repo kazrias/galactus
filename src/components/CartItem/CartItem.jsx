4d1d00a5d39df500106ed3a78316d9a5bc89fb05
kazrias <ka6rias@gmail.com>
2023-12-12 21:42:06 +0400

    Add cart item delete btn

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,14 +1,17 @@
 import './CartItem.css'
+import X from '../../images/x-symbol-svgrepo-com.svg'
 export const CartItem = ({ setCartItems, cartItems, name, price, id, images }) => {
   return (
     <div className='cart-item'>
       <div className='cart-item__img'>
         <img src={images.first} alt="" />
       </div>
-      <div className="cart-item__info">
-        <p className="cart-item__name">{name}</p>
-        <p className="cart-item__price">{price}$</p>
+      <div className='cart-item__info'>
+        <p className='cart-item__name'>{name}</p>
+        <p className='cart-item__price'>{price}$</p>
+
       </div>
+      <button className='cart-item__btn'><img className='cart-item__btn-img' src={X} alt="" /></button>
     </div>
   )
 }
