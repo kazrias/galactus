55aab6b495144d17369ea7da18d7fd72e9c35c23
kazrias <ka6rias@gmail.com>
2023-12-21 19:47:57 +0400

    Fix scroll and add Total component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,20 +2,23 @@ import './Cart.css'
 import { Ufo } from '../Ufo/Ufo';
 import { CartItem } from '../CartItem/CartItem';
 import { Space } from '../Space/Space';
+import { Total } from '../Total/Total';
 export const Cart = ({ isCartClosing, isCartOpened, setCartItems, cartItems = [] }) => {
   return (
     isCartOpened && <div className="cart-wrapper">
       < div className={`overlay ${isCartClosing ? 'closing' : ''}`} ></div >
       <div className={`cart ${isCartClosing ? 'closing' : ''}`}>
 
-        {
-          cartItems.length ? cartItems.map(obj => (
-            <CartItem setCartItems={setCartItems} cartItems={cartItems} key={obj.id} {...obj} />
-          )) : <Ufo />
-        }
+        <div className="cart-items">
+          {
+            cartItems.length ? cartItems.map(obj => (
+              <CartItem setCartItems={setCartItems} cartItems={cartItems} key={obj.id} {...obj} />
+            )) : <Ufo />
+          }
+        </div>
         <Space />
+        <Total />
       </div>
-
     </div>
   )
 }
---
+++ src/components/Total/Total.jsx
@@ -0,0 +1,11 @@
+import './Total.css'
+export const Total = () => {
+
+  return (
+    <div className="total">
+      <p className='total-text'>Total</p>
+      <div></div>
+      <p className="total-money">3000$</p>
+    </div>
+  )
+}
