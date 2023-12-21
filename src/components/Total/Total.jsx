ec3ed24ad20f4c79ebcf03fc75eddbde232a318e
kazrias <ka6rias@gmail.com>
2023-12-21 20:05:27 +0400

    Add total block with btn

--- src/components/Total/Total.jsx
+++ src/components/Total/Total.jsx
@@ -2,10 +2,13 @@ import './Total.css'
 export const Total = () => {
 
   return (
-    <div className="total">
-      <p className='total-text'>Total</p>
-      <div></div>
-      <p className="total-money">3000$</p>
+    <div className='total-box'>
+      <div className="total">
+        <p className='total-text'>Total</p>
+        <div></div>
+        <p className='total-money'>3000$</p>
+      </div>
+      <button className='total-btn'>Order</button>
     </div>
   )
 }
