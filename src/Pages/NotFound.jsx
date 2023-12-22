7b5722ea75de7799ff1a8868240ae384af7d5477
kazrias <ka6rias@gmail.com>
2023-12-23 01:47:56 +0400

    Add responsive

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import './NotFound.css'
 export const NotFound = () => {
   return (
-    <div className="container" style={{ position: "absolute", inset: '0' }}>
+    <div className="container container--notFound" style={{ position: "absolute", inset: '0' }}>
       <div style={{ position: 'relative', height: '100%', textAlign: 'center', fontFamily: 'MBF Inno', color: '#fff', letterSpacing: '15px', fontSize: '36px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
         <h2 >Oops, looks like you're lost in space!</h2>
-        <p style={{ fontSize: '180px' }}>404</p>
-        <p style={{textDecoration:'underline'}}><Link to='/'>Go home!</Link></p>
+        <h3 style={{ fontSize: '180px' }}>404</h3>
+        <p style={{ textDecoration: 'underline' }}><Link to='/'>Go home!</Link></p>
       </div>
     </div>
   )
