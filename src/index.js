c421f93b5f7a969a2e18cf966746d3667738e1e8
kazrias <ka6rias@gmail.com>
2024-03-08 04:09:40 +0400

    Add redux provider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import React from 'react'
+
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+
 import { createRoot } from 'react-dom/client';
 import App from './App.js';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,7 +10,9 @@ import { BrowserRouter } from 'react-router-dom';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
