dd9296a3056c81fe5fa4d0bec37b8c5c982e356f
Artyom Harutyunyan <ka6rias@gmail.com>
2024-03-17 18:49:11 +0400

    Add logout functionality

--- src/components/ProfileMenu/ProfileMenu.jsx
+++ src/components/ProfileMenu/ProfileMenu.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useRef, useState } from 'react'
+import { auth } from '../../config/firebaseConfig'
+import { signOut } from 'firebase/auth'
+import { loginUser } from '../../store/slices/appSlice'
+import { UseDispatch, useDispatch } from 'react-redux'
 import './ProfileMenu.css'
 
-export const ProfileMenu = () => {
+export const ProfileMenu = ({ profileClicked }) => {
+  const [outClicked, setOutClicked] = useState(false);
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const signOutUser = async () => {
+      await signOut(auth).then(() => {
+        // Sign-out successful.
+      }).catch((error) => {
+        // An error happened.
+      });
+      dispatch(loginUser({ logged: false, data: '' }))
+      setOutClicked(false)
+    }
+    if (outClicked) signOutUser()
+  }, [outClicked])
   return (
-    <ul className="profile-menu">
-      <li className="profile-meni__item"><button className="profile-meni__item-btn">Log Out</button></li>
+    <ul className={`profile-menu ${profileClicked ? 'active' : ''}`}>
+      <li onClick={() => setOutClicked(true)} className="profile-meni__item"><button className="profile-meni__item-btn">Log Out</button></li>
     </ul>
   )
 }
