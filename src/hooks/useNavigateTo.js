757203d7d1c3cbd6be24ef67f8feb811eda45d63
Artyom Harutyunyan <ka6rias@gmail.com>
2024-03-19 22:07:36 +0400

    Fix

--- src/hooks/useNavigateTo.js
+++ src/hooks/useNavigateTo.js
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-
 export const useNavigateTo = () => {
   const navigate = useNavigate();
   const { logged } = useSelector((state) => state.app.loggedUser);
